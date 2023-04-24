27d94f0f0f8b76cb608f6b4f81761320de3b66ad
Mingyu Jin <jmingyu@gmail.com>
2023-04-24 22:08:18 +0900

    Re-structure

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+from ._installer import InstallManager
+
+def get_window():
+    from ._window import MainWindow
+    return MainWindow()
---
+++ core/_installer.py
@@ -0,0 +1,12 @@
+import yaml
+import semver
+
+class InstallManager:
+    def check_new_version(self):
+        return False
+
+    def update(self):
+        pass
+
+if __name__ == "__main__":
+    print("Wrong access")
