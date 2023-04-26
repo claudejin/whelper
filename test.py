223061b861912967217db3977ea4e3ea2d442c7b
Mingyu Jin <2018409@vieworks.com>
2023-04-26 22:04:59 +0900

    Remove unused file

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests  # request img from web
-from os import makedirs
-from os.path import dirname
-
-new_file = f"https://raw.githubusercontent.com/claudejin/whelper/main/whelper.pyw"
-res = requests.get(new_file, headers={"Cache-Control": "no-cache"})
-if res.status_code == 200:
-    makedirs(dirname("hi/new_file"), exist_ok=True)
